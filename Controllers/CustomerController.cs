7d62fdb923f8a9e38068c1a9d2c7f0625b14bf90
DanielleSCorreia <danielle.santana@gmail.com>
2019-06-04 21:45:30 -0300

    Customer
    Inclusão das informações para Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (CustomerModel model = new CustomerModel())
+            {
+                IList<Customer> lista = model.Read();
+                return View(lista);
+            }                
+        }
+
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Northwind.Models
+{
+    public class CustomerModel : IDisposable
+    {
+        private SqlConnection connection;
+
+        public CustomerModel()
+        {
+            string strConn = @"Data Source =DESKTOP-HE7ITUQ\SQLEXPRESS; Initial Catalog=Northwind;Integrated Security=true;Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";                       
+            connection = new SqlConnection(strConn);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+
+        public void Create(Customer customer)
+        {
+
+        }
+
+        public IList<Customer> Read()
+        {
+            IList<Customer> customers = new List<Customer>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "Select * from Customers";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+             Customer customer = new Customer();                
+             customer.CustomerID    = (string)reader["CustomerID"];
+             customer.CompanyName   = (string)reader["CompanyName"];
+             customer.ContactName   = (string)reader["ContactName"];
+             customer.ContactTitle  = (string)reader["ContactTitle"];
+             customer.Address       = (string)reader["Address"];
+             customer.City          = (string)reader["City"];
+             customer.Region        = reader["Region"].GetType().ToString() == "System.DBNull" ? string.Empty : (string)reader["Region"]; 
+             customer.PostalCode    = reader["PostalCode"].GetType().ToString() == "System.DBNull" ? string.Empty: (string)reader["PostalCode"];
+             customer.Country       = (string)reader["Country"];
+             customer.Phone         = (string)reader["Phone"];
+             customer.Fax           = reader["Fax"].GetType().ToString() == "System.DBNull" ? string.Empty : (string)reader["Fax"];
+
+            customers.Add(customer);
+            }
+
+            return customers;
+
+        }
+
+
+    }
+}
